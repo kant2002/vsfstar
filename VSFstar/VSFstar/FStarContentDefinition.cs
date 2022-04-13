f99656bd66d84a5544c5640a24cc70637c86a823
Andrii Kurdiumov <kant2002@gmail.com>
2022-04-13 14:47:47 +0600

    Add very rough VS integration Has issues with spawning a lot of fstar.exe processes.

---
+++ VSFstar/VSFstar/FStarContentDefinition.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.LanguageServer.Client;
+using Microsoft.VisualStudio.Utilities;
+using System.ComponentModel.Composition;
+
+namespace VSFstar
+{
+    public class FStarContentDefinition
+    {
+        [Export]
+        [Name("fst")]
+        [BaseDefinition(CodeRemoteContentDefinition.CodeRemoteContentTypeName)]
+        internal static ContentTypeDefinition FstContentTypeDefinition;
+
+        [Export]
+        [FileExtension(".fst")]
+        [ContentType("fst")]
+        internal static FileExtensionToContentTypeDefinition FstFileExtensionDefinition;
+
+        [Export]
+        [FileExtension(".fsti")]
+        [ContentType("fst")]
+        internal static FileExtensionToContentTypeDefinition FstiFileExtensionDefinition;
+
+    }
+}
---
+++ VSFstar/VSFstar/FStarLanguageClient.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.VisualStudio.LanguageServer.Client;
+using Microsoft.VisualStudio.Threading;
+using Microsoft.VisualStudio.Utilities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace VSFstar
+{
+    [ContentType("fst")]
+    [Export(typeof(ILanguageClient))]
+    public class FStarLanguageClient : ILanguageClient
+    {
+        public string Name => "Bar Language Extension";
+
+        public IEnumerable<string> ConfigurationSections => null;
+
+        public object InitializationOptions => null;
+
+        public IEnumerable<string> FilesToWatch => null;
+
+        public bool ShowNotificationOnInitializeFailed => true;
+
+        public event AsyncEventHandler<EventArgs> StartAsync;
+        public event AsyncEventHandler<EventArgs> StopAsync;
+
+        public async Task<Connection> ActivateAsync(CancellationToken token)
+        {
+            await Task.Yield();
+
+            ProcessStartInfo info = new ProcessStartInfo();
+            // info.FileName = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "Server", @"MockLanguageServer.exe");
+            info.FileName = "fstar.exe";
+            info.Arguments = "--lsp";
+            info.RedirectStandardInput = true;
+            info.RedirectStandardOutput = true;
+            info.UseShellExecute = false;
+            info.CreateNoWindow = true;
+
+            Process process = new Process();
+            process.StartInfo = info;
+
+            if (process.Start())
+            {
+                return new Connection(process.StandardOutput.BaseStream, process.StandardInput.BaseStream);
+            }
+
+            return null;
+        }
+
+        public async Task OnLoadedAsync()
+        {
+            await StartAsync.InvokeAsync(this, EventArgs.Empty);
+        }
+
+        public Task OnServerInitializeFailedAsync(Exception e)
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task OnServerInitializedAsync()
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task<InitializationFailureContext> OnServerInitializeFailedAsync(ILanguageClientInitializationInfo initializationState)
+        {
+            string message = "Oh no! F* Language Client failed to activate :(";
+            string exception = initializationState.InitializationException?.ToString() ?? string.Empty;
+            message = $"{message}\n {exception}";
+
+            var failureContext = new InitializationFailureContext()
+            {
+                FailureMessage = message,
+            };
+
+            return Task.FromResult(failureContext);
+        }
+    }
+}
